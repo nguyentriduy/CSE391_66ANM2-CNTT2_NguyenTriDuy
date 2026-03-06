67095467b487b94e07ba88a08a31ab707214d987
nguyentriduy <triduy123456789@gmail.com>
2026-03-06 11:21:18 +0700

    BTTH3

---
+++ BTTH3/lab_03_basic/main.js
@@ -0,0 +1,53 @@
+console.log("Hello from JavaScript!");
+
+let name = "Duy";
+let yearOfBirth = 2004;
+let currentYear = 2026;
+let age = currentYear - yearOfBirth;
+
+console.log("Xin chào, mình là " + name + ", năm nay mình " + age + " tuổi.");
+
+
+// TODO: Đổi giá trị score và quan sát kết quả
+let score = 6;
+
+// TODO: Dự đoán điều kiện if/else đang làm gì, rồi chạy thử
+if (score >= 8) {
+  console.log("Giỏi");
+} else if (score >= 6.5) {
+  console.log("Khá");
+} else if (score >= 5) {
+  console.log("Trung bình");
+} else {
+  console.log("Yếu");
+}
+
+// TODO: Viết hàm tính điểm trung bình 3 môn
+function tinhDiemTrungBinh(m1, m2, m3) {
+  let avg = (m1 + m2 + m3) / 3;
+  return avg;
+}
+
+function xepLoai(avg) {
+  if (avg >= 8) {
+    return "Giỏi";
+  } else if (avg >= 6.5) {
+    return "Khá";
+  } else if (avg >= 5) {
+    return "Trung bình";
+  } else {
+    return "Yếu";
+  }
+}
+
+function kiemTraTuoi(age) {
+  if (age >= 18) {
+    console.log("Đủ 18 tuổi");
+  } 
+  else {
+    console.log("Chưa đủ 18 tuổi");
+  }
+}
+
+kiemTraTuoi(16);
+kiemTraTuoi(20);
